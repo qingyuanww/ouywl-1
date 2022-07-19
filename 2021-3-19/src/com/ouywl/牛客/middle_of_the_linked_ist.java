8a0fa281e7e83aa29c1ca8ba1928803b1995bb05
oywl <2410667597@qq.com>
2022-07-19 14:51:38 +0800

    快慢指针问题，经典的思想

---
+++ 2021-3-19/src/com/ouywl/牛客/middle_of_the_linked_ist.java
@@ -0,0 +1,18 @@
+package com.ouywl.牛客;
+
+/** 链表的中间节点
+ * @description:https://leetcode.cn/problems/middle-of-the-linked-list/
+ * @author: oywl
+ * @time: 2022-7-19 14:50
+ */
+
+public class middle_of_the_linked_ist {
+    public ListNode middleNode(ListNode head) {
+        ListNode p =head,q=head;
+        while(q!=null&&q.next!=null){
+            q=q.next.next;
+            p=p.next;
+        }
+        return p;
+    }
+}
