37912d275d5468d4f592c09be080ff8205d907f3
oywl <2410667597@qq.com>
2023-06-28 15:32:50 +0800

    编写一个Java函数，通过调用A.get()、B.get()、C.get()三个接口，获取三个整数，然后将这三个整数累加，最终返回累加的值。要求： 1.调用三个接口的操作需要并行执行，以提高效率； 2.累加操作需要在获取三个整数的操作完成后进行，因此需要保证三个整数均已获取后才能进行累加操作； 3.考虑多线程安全问题。

--- ThreadTest/src/main/java/com/oywl/Thread/CountDownLatchTest.java
+++ ThreadTest/src/main/java/com/oywl/Thread/CountDownLatchTest.java
@@ -1,7 +1,12 @@
 package com.oywl.Thread;
 
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @description: CountDownLatch 是 所有的其他线程结束后，才开始执行主线程, 常用与计数器 ，英雄联盟五个玩家准备好后才能开始游戏
@@ -10,36 +15,53 @@ import java.util.concurrent.CountDownLatch;
  */
 
 public class CountDownLatchTest {
-    public static void main(String[] args) throws InterruptedException {
-        CountDownLatch downLatch = new CountDownLatch(5);
-
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        long timeMillis = System.currentTimeMillis();
+        System.out.println(timeMillis);
+        int threadCount =5;
+        AtomicInteger atomicCounter = new AtomicInteger(threadCount); // 使用AtomicInteger作为计数器
+        CountDownLatch downLatch = new CountDownLatch(threadCount);
+        ExecutorService executor = Executors.newFixedThreadPool(threadCount);
+        List<Future<Integer>> futures = new ArrayList<>();
         for (int i = 0; i < downLatch.getCount(); i++) {
-            new Thread(new MyThread(downLatch)).start();
+            Future future = executor.submit(new MyThread(downLatch,atomicCounter));
+            futures.add(future);
         }
         System.out.println("正在等待所有玩家准备好");
         //开始让 当前线程 等待，直到downLatch的计数器为0
         downLatch.await();
+        // 获取子线程的执行结果
+        for (Future<Integer> future : futures) {
+            System.out.println(future.get());
+        }
+        executor.shutdown();//关闭线程池
         System.out.println("准备好,开始游戏");
+        System.out.println((System.currentTimeMillis()-timeMillis)/1000.0+"秒");
     }
 
-    private static class MyThread implements Runnable{
+    private static class MyThread implements Callable {
         private CountDownLatch downLatch;
 
-        public MyThread(CountDownLatch downLatch) {
+        private AtomicInteger atomicInteger;
+
+        public MyThread(CountDownLatch downLatch,AtomicInteger atomicInteger) {
             this.downLatch = downLatch;
+            this.atomicInteger=atomicInteger;
         }
 
         @Override
-        public void run() {
+        public Object call() throws Exception {
+            Random rand = new Random();
+            int randomNum = rand.nextInt((3000 - 1000) + 1) + 1000;//产生1000到3000之间的随机整数
             try {
-                Random rand = new Random();
-                int randomNum = rand.nextInt((3000 - 1000) + 1) + 1000;//产生1000到3000之间的随机整数
                 Thread.sleep(randomNum);
-                System.out.println(Thread.currentThread().getName()+" 已经准备好了, 所使用的时间为 "+((double)randomNum/1000)+"s");
+                System.out.println(Thread.currentThread().getName()+" 已经准备好了, 所使用的时间为 "+((double)randomNum/1000.0)+"s");
                 downLatch.countDown();
+                atomicInteger.decrementAndGet();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            return randomNum;
         }
     }
 }
